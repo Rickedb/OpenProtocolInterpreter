6edeb71e1f47ec56bcd780cac946405f9625415c
Henrique Dal Bello <henrique.d.bello@gmail.com>
2018-01-22 10:30:30 -0200

    Sample initial commit

---
+++ sample/OpenProtocolInterpreter.Sample/DriverForm.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace OpenProtocolInterpreter.Sample
+{
+    partial class DriverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.connectionStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolstripLast = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lastMessageArrived = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnection = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnTighteningSubscribe = new System.Windows.Forms.Button();
+            this.btnJobInfoSubscribe = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionStatus,
+            this.toolstripLast,
+            this.lastMessageArrived});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 268);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(653, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // connectionStatus
+            // 
+            this.connectionStatus.BackColor = System.Drawing.Color.Red;
+            this.connectionStatus.Name = "connectionStatus";
+            this.connectionStatus.Size = new System.Drawing.Size(79, 17);
+            this.connectionStatus.Text = "Disconnected";
+            // 
+            // toolstripLast
+            // 
+            this.toolstripLast.Name = "toolstripLast";
+            this.toolstripLast.Size = new System.Drawing.Size(96, 17);
+            this.toolstripLast.Text = "Last Mid Arrived:";
+            // 
+            // lastMessageArrived
+            // 
+            this.lastMessageArrived.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lastMessageArrived.Name = "lastMessageArrived";
+            this.lastMessageArrived.Size = new System.Drawing.Size(463, 17);
+            this.lastMessageArrived.Spring = true;
+            this.lastMessageArrived.Text = "Last MID";
+            // 
+            // textIp
+            // 
+            this.textIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textIp.Location = new System.Drawing.Point(36, 12);
+            this.textIp.Name = "textIp";
+            this.textIp.Size = new System.Drawing.Size(160, 22);
+            this.textIp.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // btnConnection
+            // 
+            this.btnConnection.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnection.Location = new System.Drawing.Point(385, 10);
+            this.btnConnection.Name = "btnConnection";
+            this.btnConnection.Size = new System.Drawing.Size(100, 23);
+            this.btnConnection.TabIndex = 4;
+            this.btnConnection.Text = "Connect";
+            this.btnConnection.UseVisualStyleBackColor = true;
+            this.btnConnection.Click += new System.EventHandler(this.btnConnection_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(209, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Port:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(250, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1215752192,
+            23,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4545,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnTighteningSubscribe);
+            this.groupBox1.Controls.Add(this.btnJobInfoSubscribe);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(629, 85);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Subscribes";
+            // 
+            // btnTighteningSubscribe
+            // 
+            this.btnTighteningSubscribe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTighteningSubscribe.Location = new System.Drawing.Point(115, 21);
+            this.btnTighteningSubscribe.Name = "btnTighteningSubscribe";
+            this.btnTighteningSubscribe.Size = new System.Drawing.Size(103, 51);
+            this.btnTighteningSubscribe.TabIndex = 11;
+            this.btnTighteningSubscribe.Text = "Tightening Subscribe";
+            this.btnTighteningSubscribe.UseVisualStyleBackColor = true;
+            this.btnTighteningSubscribe.Click += new System.EventHandler(this.btnTighteningSubscribe_Click);
+            // 
+            // btnJobInfoSubscribe
+            // 
+            this.btnJobInfoSubscribe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJobInfoSubscribe.Location = new System.Drawing.Point(6, 21);
+            this.btnJobInfoSubscribe.Name = "btnJobInfoSubscribe";
+            this.btnJobInfoSubscribe.Size = new System.Drawing.Size(103, 51);
+            this.btnJobInfoSubscribe.TabIndex = 10;
+            this.btnJobInfoSubscribe.Text = "Job Info Subscribe";
+            this.btnJobInfoSubscribe.UseVisualStyleBackColor = true;
+            this.btnJobInfoSubscribe.Click += new System.EventHandler(this.btnJobInfoSubscribe_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(629, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Commands";
+            // 
+            // DriverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 290);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnConnection);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textIp);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "DriverForm";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel connectionStatus;
+        private System.Windows.Forms.ToolStripStatusLabel toolstripLast;
+        private System.Windows.Forms.TextBox textIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnection;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnTighteningSubscribe;
+        private System.Windows.Forms.Button btnJobInfoSubscribe;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ToolStripStatusLabel lastMessageArrived;
+    }
+}
+
---
+++ sample/OpenProtocolInterpreter.Sample/DriverForm.cs
@@ -0,0 +1,162 @@
+﻿using OpenProtocolInterpreter.MIDs;
+using OpenProtocolInterpreter.Sample.Driver;
+using OpenProtocolInterpreter.Sample.Driver.Events;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using OpenProtocolInterpreter.Sample.Driver.Helpers;
+using OpenProtocolInterpreter.MIDs.KeepAlive;
+using System.Drawing;
+
+namespace OpenProtocolInterpreter.Sample
+{
+    public partial class DriverForm : Form
+    {
+        private Timer keepAliveTimer;
+        private OpenProtocolDriver driver;
+
+        public DriverForm()
+        {
+            InitializeComponent();
+            this.keepAliveTimer = new Timer();
+            this.keepAliveTimer.Tick += KeepAliveTimer_Tick;
+            this.keepAliveTimer.Interval = 1000;
+        }
+
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+            //Added list of mids i want to use in my interpreter, every another will be desconsidered
+            this.driver = new OpenProtocolDriver(new List<MID>()
+            {
+                new MIDs.Communication.MID_0002(),
+                new MIDs.Communication.MID_0005(),
+                new MIDs.Communication.MID_0004(),
+                new MIDs.Communication.MID_0003(),
+
+                new MIDs.ParameterSet.MID_0011(),
+                new MIDs.ParameterSet.MID_0013(),
+
+                new MIDs.Job.MID_0035(),
+                new MIDs.Job.MID_0031(),
+
+                new MIDs.Alarm.MID_0071(),
+                new MIDs.Alarm.MID_0074(),
+                new MIDs.Alarm.MID_0076(),
+
+                new MIDs.VIN.MID_0052(),
+
+                new MIDs.Tightening.MID_0061(),
+                new MIDs.Tightening.MID_0065(),
+
+                new MIDs.Time.MID_0081(),
+
+                new MID_9999()
+            });
+
+            if (this.driver.BeginCommunication(new Ethernet.SimpleTcpClient().Connect(this.textIp.Text, 4545)))
+            {
+                this.keepAliveTimer.Start();
+                this.connectionStatus.Text = "Connected!";
+                this.connectionStatus.BackColor = Color.Green;
+            }
+            else
+            {
+                this.driver = null;
+                this.connectionStatus.Text = "Disconnected!";
+                this.connectionStatus.BackColor = Color.Red;
+            }
+        }
+
+        private void KeepAliveTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.driver.keepAlive.ElapsedMilliseconds == 10000) //10 sec
+            {
+                Console.Write($"Sending Keep Alive...");
+                var pack = this.driver.sendAndWaitForResponse(new MID_9999().buildPackage(), TimeSpan.FromSeconds(10));
+                if (pack != null && pack.HeaderData.Mid == MID_9999.MID)
+                {
+                    lastMessageArrived.Text = MID_9999.MID.ToString();
+                    Console.Write($"Keep Alive Received");
+                }
+                else
+                    Console.Write($"Keep Alive Not Received");
+            }
+        }
+
+        /// <summary>
+        /// Job info subscribe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnJobInfoSubscribe_Click(object sender, EventArgs e)
+        {
+            this.driver.AddUpdateOnReceivedCommand(typeof(MIDs.Job.MID_0035), this.onJobInfoReceived);
+        }
+
+        /// <summary>
+        /// Tightening subscribe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTighteningSubscribe_Click(object sender, EventArgs e)
+        {
+            this.driver.AddUpdateOnReceivedCommand(typeof(MIDs.Tightening.MID_0060), this.onTighteningReceived);
+        }
+
+        /// <summary>
+        /// Async method from controller, MID 0035 (Job Info)
+        /// </summary>
+        /// <param name="e"></param>
+        private void onJobInfoReceived(MIDIncome e)
+        {
+            this.driver.sendMessage(e.Mid.BuildAckPackage());
+            
+            var jobInfo = e.Mid as MIDs.Job.MID_0035;
+            lastMessageArrived.Text = MIDs.Job.MID_0035.MID.ToString();
+            Console.WriteLine($@"JOB INFO RECEIVED (MID 0035): 
+                                 Job ID: <{jobInfo.JobID}>
+                                 Job Status: <{(int)jobInfo.JobStatus}> ({jobInfo.JobStatus.ToString()})
+                                 Job Batch Mode: <{(int)jobInfo.JobBatchMode}> ({jobInfo.JobBatchMode.ToString()})
+                                 Job Batch Size: <{jobInfo.JobBatchSize}>
+                                 Job Batch Counter: <{jobInfo.JobBatchCounter}>
+                                 TimeStamp: <{jobInfo.TimeStamp.ToString("yyyy-MM-dd:HH:mm:ss")}>");
+        }
+
+        /// <summary>
+        /// Async method from controller, MID 0035 (Job Info)
+        /// </summary>
+        /// <param name="e"></param>
+        private void onTighteningReceived(MIDIncome e)
+        {
+            this.driver.sendMessage(e.Mid.BuildAckPackage());
+
+            var tighteningMid = e.Mid as MIDs.Tightening.MID_0061;
+            lastMessageArrived.Text = MIDs.Tightening.MID_0061.MID.ToString();
+            Console.WriteLine($@"TIGHTENING RECEIVED (MID 0061): 
+                                 Cell ID: <{tighteningMid.CellID}>
+                                 Channel ID: <{tighteningMid.ChannelID}>
+                                 Torque Controller Name: <{tighteningMid.TorqueControllerName}>
+                                 VIN Number: <{tighteningMid.VINNumber}>
+                                 Job ID: <{tighteningMid.JobID}>
+                                 Parameter Set ID: <{tighteningMid.ParameterSetID}>
+                                 Batch Size: <{tighteningMid.BatchSize}>
+                                 Batch Counter: <{tighteningMid.BatchCounter}>
+                                 Tightening Status: <{tighteningMid.TighteningStatus}>
+                                 Torque Status: <{(int)tighteningMid.TorqueStatus}> ({tighteningMid.TorqueStatus.ToString()})
+                                 Angle Status: <{(int)tighteningMid.AngleStatus}> ({tighteningMid.AngleStatus.ToString()})
+                                 Torque Min Limit: <{tighteningMid.TorqueMinLimit}>
+                                 Torque Max Limit: <{tighteningMid.TorqueMaxLimit}>
+                                 Torque Final Target: <{tighteningMid.TorqueFinalTarget}>
+                                 Torque: <{tighteningMid.Torque}>
+                                 Angle Min Limit: <{tighteningMid.AngleMinLimit}>
+                                 Angle Max Limit: <{tighteningMid.AngleMaxLimit}>
+                                 Final Angle Target: <{tighteningMid.AngleFinalTarget}>
+                                 Angle: <{tighteningMid.Angle}>
+                                 TimeStamp: <{tighteningMid.TimeStamp.ToString("yyyy-MM-dd:HH:mm:ss")}>
+                                 Last Change In Parameter Set: <{tighteningMid.LastChangeInParameterSet.ToString("yyyy-MM-dd:HH:mm:ss")}>
+                                 Batch Status: <{(int)tighteningMid.BatchStatus}> ({tighteningMid.BatchStatus.ToString()})
+                                 TighteningID: <{tighteningMid.TighteningID}>");
+        }
+
+    }
+}
