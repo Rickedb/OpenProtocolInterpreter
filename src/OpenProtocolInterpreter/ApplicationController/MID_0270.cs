7a20ab7b9a0ad14d8142fa57acdd07b5b11fcb50
Henrique Dal Bello <henrique.d.bello@gmail.com>
2018-05-14 01:39:43 -0300

    ApplicationController all revisions Done

--- src/OpenProtocolInterpreter/ApplicationController/MID_0270.cs
+++ src/OpenProtocolInterpreter/ApplicationController/MID_0270.cs
@@ -12,25 +12,12 @@
     /// </summary>
     public class MID_0270 : Mid
     {
-        private const int length = 20;
+        private const int LAST_REVISION = 1;
         public const int MID = 270;
-        private const int revision = 1;
 
-        public MID_0270() : base(length, MID, revision) { }
+        public MID_0270() : base(MID, LAST_REVISION) { }
 
-        internal MID_0270(IMid nextTemplate) : base(length, MID, revision)
-        {
-            NextTemplate = nextTemplate;
-        }
-
-        public override Mid Parse(string package)
-        {
-            if (base.IsCorrectType(package))
-                return (MID_0270)base.Parse(package);
-
-            return NextTemplate.Parse(package);
-        }
-
-        protected override void RegisterDatafields() { }
+        internal MID_0270(IMid nextTemplate) : this() => NextTemplate = nextTemplate;
+        
     }
 }
