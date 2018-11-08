585ffbd86cdd694902c8b86b7a68fba609937406
Henrique Dal Bello <henrique.d.bello@gmail.com>
2018-11-07 23:41:54 -0200

    appveyor update for NuGet publishing

--- src/OpenProtocolInterpreter/Properties/AssemblyInfo.cs
+++ src/OpenProtocolInterpreter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpenProtocolInterpreter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Converts the ugly package that came from Open Protocol TCP/IP or Serial to an object")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Henrique Dal Bello Batista")]
 [assembly: AssemblyProduct("OpenProtocolInterpreter")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
