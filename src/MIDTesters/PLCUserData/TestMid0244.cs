b15e85450793de7f3ec38129a9f928bce1635d8a
Henrique Dal Bello <henrique.d.bello@gmail.com>
2019-04-02 13:13:56 -0300

    PLC User Data byte unit tests

--- src/MIDTesters/PLCUserData/TestMid0244.cs
+++ src/MIDTesters/PLCUserData/TestMid0244.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenProtocolInterpreter.PLCUserData;
 
@@ -15,6 +16,17 @@ namespace MIDTesters.PLCUserData
 
             Assert.AreEqual(typeof(Mid0244), mid.GetType());
             Assert.AreEqual(package, mid.Pack());
+        }
+
+        [TestMethod]
+        public void Mid0244ByteRevision1()
+        {
+            string package = "00200244            ";
+            byte[] bytes = GetAsciiBytes(package);
+            var mid = _midInterpreter.Parse(bytes);
+
+            Assert.AreEqual(typeof(Mid0244), mid.GetType());
+            Assert.IsTrue(mid.PackBytes().SequenceEqual(bytes));
         }
     }
 }
--- src/MIDTesters/PLCUserData/TestMid0245.cs
+++ src/MIDTesters/PLCUserData/TestMid0245.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenProtocolInterpreter.PLCUserData;
 
@@ -17,6 +18,19 @@ namespace MIDTesters.PLCUserData
             Assert.IsNotNull(mid.Offset);
             Assert.IsNotNull(mid.UserData);
             Assert.AreEqual(package, mid.Pack());
+        }
+
+        [TestMethod]
+        public void Mid0245ByteRevision1()
+        {
+            string package = "00460245            022My identifier less than";
+            byte[] bytes = GetAsciiBytes(package);
+            var mid = _midInterpreter.Parse<Mid0245>(bytes);
+
+            Assert.AreEqual(typeof(Mid0245), mid.GetType());
+            Assert.IsNotNull(mid.Offset);
+            Assert.IsNotNull(mid.UserData);
+            Assert.IsTrue(mid.PackBytes().SequenceEqual(bytes));
         }
     }
 }
