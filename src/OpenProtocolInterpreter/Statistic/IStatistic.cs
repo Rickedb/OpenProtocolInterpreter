737fe537f9cd6e300c54055b60b763cc3b8ca017
Henrique Dal Bello <henrique.d.bello@gmail.com>
2020-02-17 00:25:12 -0300

    Statistic summaries

--- src/OpenProtocolInterpreter/Statistic/IStatistic.cs
+++ src/OpenProtocolInterpreter/Statistic/IStatistic.cs
@@ -1,5 +1,8 @@
 ﻿namespace OpenProtocolInterpreter.Statistic
 {
+    /// <summary>
+    /// Statistic message category. Every statistic mid must implement <see cref="IStatistic"/>.
+    /// </summary>
     public interface IStatistic
     {
     }
