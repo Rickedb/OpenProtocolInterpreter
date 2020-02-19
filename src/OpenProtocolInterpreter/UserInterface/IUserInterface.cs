0824f874cf8de0bcb1e820368acdb5a1a5ce81da
Henrique Dal Bello <henrique.d.bello@gmail.com>
2020-02-18 21:09:15 -0300

    User interface summaries

--- src/OpenProtocolInterpreter/UserInterface/IUserInterface.cs
+++ src/OpenProtocolInterpreter/UserInterface/IUserInterface.cs
@@ -1,5 +1,8 @@
 ﻿namespace OpenProtocolInterpreter.UserInterface
 {
+    /// <summary>
+    /// User interface message category. Every user interface must implement <see cref="IUserInterface"/>.
+    /// </summary>
     public interface IUserInterface
     {
     }
