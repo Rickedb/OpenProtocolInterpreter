96f76207a96248075326d741e738dd7ceeefe91e
Henrique Dal Bello <henrique.d.bello@gmail.com>
2020-09-14 21:45:27 -0300

    Fixed mid 38 revision 2 not packing

--- src/OpenProtocolInterpreter/Job/Mid0038.cs
+++ src/OpenProtocolInterpreter/Job/Mid0038.cs
@@ -61,7 +61,8 @@ namespace OpenProtocolInterpreter.Job
                             {
                                 new DataField((int)DataFields.JOB_ID, 20, 2, '0', DataField.PaddingOrientations.LEFT_PADDED, false),
                             }
-                }
+                },
+                { 2, new List<DataField>() }
             };
         }
 
