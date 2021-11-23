6c036dfa0795a381971fd8540628bae1c4bf2342
Henrique Dal Bello <henrique.d.bello@gmail.com>
2021-11-23 01:16:43 -0300

    Tool Mids updated

--- src/MIDTesters/Tool/TestMid0040.cs
+++ src/MIDTesters/Tool/TestMid0040.cs
@@ -8,7 +8,7 @@ namespace MIDTesters.Tool
     public class TestMid0040 : MidTester
     {
         [TestMethod]
-        public void Mid0040AllRevisions()
+        public void Mid0040Revisions1To5()
         {
             string package = "00200040004         ";
             var mid = _midInterpreter.Parse(package);
@@ -18,13 +18,36 @@ namespace MIDTesters.Tool
         }
 
         [TestMethod]
-        public void Mid0040ByteAllRevisions()
+        public void Mid0040ByteRevisions1To5()
         {
             string package = "00200040004         ";
             byte[] bytes = GetAsciiBytes(package);
             var mid = _midInterpreter.Parse(bytes);
 
             Assert.AreEqual(typeof(Mid0040), mid.GetType());
+            Assert.IsTrue(mid.PackBytes().SequenceEqual(bytes));
+        }
+
+        [TestMethod]
+        public void Mid0040Revisions6And7()
+        {
+            string package = "00260040007         010001";
+            var mid = _midInterpreter.Parse<Mid0040>(package);
+
+            Assert.AreEqual(typeof(Mid0040), mid.GetType());
+            Assert.IsNotNull(mid.ToolNumber);
+            Assert.AreEqual(package, mid.Pack());
+        }
+
+        [TestMethod]
+        public void Mid0040ByteRevisions6And7()
+        {
+            string package = "00260040007         010001";
+            byte[] bytes = GetAsciiBytes(package);
+            var mid = _midInterpreter.Parse<Mid0040>(bytes);
+
+            Assert.AreEqual(typeof(Mid0040), mid.GetType());
+            Assert.IsNotNull(mid.ToolNumber);
             Assert.IsTrue(mid.PackBytes().SequenceEqual(bytes));
         }
     }
--- src/OpenProtocolInterpreter/Tool/Mid0045.cs
+++ src/OpenProtocolInterpreter/Tool/Mid0045.cs
@@ -18,35 +18,57 @@ namespace OpenProtocolInterpreter.Tool
     {
         private readonly IValueConverter<decimal> _decimalConverter;
         private readonly IValueConverter<int> _intConverter;
-        private const int LAST_REVISION = 1;
+        private const int LAST_REVISION = 2;
         public const int MID = 45;
 
         public CalibrationUnit CalibrationValueUnit
         {
-            get => (CalibrationUnit)GetField(1,(int)DataFields.CALIBRATION_VALUE_UNIT).GetValue(_intConverter.Convert);
-            set => GetField(1,(int)DataFields.CALIBRATION_VALUE_UNIT).SetValue(_intConverter.Convert, (int)value);
+            get => (CalibrationUnit)GetField(1, (int)DataFields.CALIBRATION_VALUE_UNIT).GetValue(_intConverter.Convert);
+            set => GetField(1, (int)DataFields.CALIBRATION_VALUE_UNIT).SetValue(_intConverter.Convert, (int)value);
         }
         public decimal CalibrationValue
         {
-            get => GetField(1,(int)DataFields.CALIBRATION_VALUE).GetValue(_decimalConverter.Convert);
-            set => GetField(1,(int)DataFields.CALIBRATION_VALUE).SetValue(_decimalConverter.Convert, value);
+            get => GetField(1, (int)DataFields.CALIBRATION_VALUE).GetValue(_decimalConverter.Convert);
+            set => GetField(1, (int)DataFields.CALIBRATION_VALUE).SetValue(_decimalConverter.Convert, value);
+        }
+        public int ChannelNumber
+        {
+            get => GetField(2, (int)DataFields.CHANNEL_NUMBER).GetValue(_intConverter.Convert);
+            set => GetField(2, (int)DataFields.CHANNEL_NUMBER).SetValue(_intConverter.Convert, value);
         }
 
-        public Mid0045() : base(MID, LAST_REVISION)
+        public Mid0045() : this(LAST_REVISION)
+        {
+        }
+
+        public Mid0045(int revision = LAST_REVISION) : base(MID, revision)
         {
             _decimalConverter = new DecimalTrucatedConverter(2);
             _intConverter = new Int32Converter();
         }
 
         /// <summary>
-        /// Revision 1 Constructor
+        /// Revision 1 constructor
         /// </summary>
         /// <param name="calibrationValueUnit">The unit in which the calibration value is sent. The calibration value unit is one byte long and specified by one ASCII digit.</param>
         /// <param name="calibrationValue">The calibration value is multiplied by 100 and sent as an integer (2 decimals truncated). The calibration value is six bytes long and is specified by six ASCII digits.</param>
-        public Mid0045(CalibrationUnit calibrationValueUnit, decimal calibrationValue) : this()
+        /// <param name="revision">Revision</param>
+        public Mid0045(CalibrationUnit calibrationValueUnit, decimal calibrationValue, int revision = 1) : this(revision)
         {
             CalibrationValueUnit = calibrationValueUnit;
             CalibrationValue = calibrationValue;
+        }
+
+        /// <summary>
+        /// Revision 2 constructor
+        /// </summary>
+        /// <param name="calibrationValueUnit">The unit in which the calibration value is sent. The calibration value unit is one byte long and specified by one ASCII digit.</param>
+        /// <param name="calibrationValue">The calibration value is multiplied by 100 and sent as an integer (2 decimals truncated). The calibration value is six bytes long and is specified by six ASCII digits.</param>
+        /// <param name="channelNumber">The number of the channel to set the calibration value.</param>
+        /// <param name="revision">Revision</param>
+        public Mid0045(CalibrationUnit calibrationValueUnit, decimal calibrationValue, int channelNumber, int revision = 2) : this(calibrationValueUnit, calibrationValue, revision)
+        {
+            ChannelNumber = channelNumber;
         }
 
         protected override Dictionary<int, List<DataField>> RegisterDatafields()
@@ -59,6 +81,12 @@ namespace OpenProtocolInterpreter.Tool
                                 new DataField((int)DataFields.CALIBRATION_VALUE_UNIT, 20, 1),
                                 new DataField((int)DataFields.CALIBRATION_VALUE, 23, 6, '0', DataField.PaddingOrientations.LEFT_PADDED)
                             }
+                },
+                {
+                    2, new List<DataField>()
+                            {
+                                new DataField((int)DataFields.CHANNEL_NUMBER, 31, 2, '0', DataField.PaddingOrientations.LEFT_PADDED),
+                            }
                 }
             };
         }
@@ -66,7 +94,8 @@ namespace OpenProtocolInterpreter.Tool
         public enum DataFields
         {
             CALIBRATION_VALUE_UNIT,
-            CALIBRATION_VALUE
+            CALIBRATION_VALUE,
+            CHANNEL_NUMBER
         }
     }
 }
