27fbb29047a30c7c131b2837012694482758a4c9
Henrique Dal Bello <henrique.d.bello@gmail.com>
2022-01-13 23:57:50 -0300

    Mid 140 revisions but no tests yet

--- src/OpenProtocolInterpreter/Converters/AdvancedJobListConverter.cs
+++ src/OpenProtocolInterpreter/Converters/AdvancedJobListConverter.cs
@@ -6,33 +6,48 @@ namespace OpenProtocolInterpreter.Converters
     public class AdvancedJobListConverter : AsciiConverter<IEnumerable<AdvancedJob>>
     {
         private readonly IValueConverter<int> _intConverter;
-        private readonly IValueConverter<bool> _boolConverter;
+        private readonly IValueConverter<bool> _boolConverter|;
         private readonly int _revision;
 
-        public AdvancedJobListConverter(IValueConverter<int> intConverter, IValueConverter<bool> boolConverter, int revision)
+        public AdvancedJobListConverter(IValueConverter<int> intConverter, int revision)
         {
             _intConverter = intConverter;
-            _boolConverter = boolConverter;
             _revision = revision;
         }
 
         public override string Convert(IEnumerable<AdvancedJob> value)
         {
-            List<string> advancedJobsList = new List<string>();
+            var advancedJobsList = new List<string>();
 
-            foreach(var job in value)
+            foreach (var job in value)
             {
-                List<string> fields = new List<string>
+                var fields = new List<string>
                 {
                     _intConverter.Convert('0', 2, DataField.PaddingOrientations.LEFT_PADDED, job.ChannelId),
                     _intConverter.Convert('0', 3, DataField.PaddingOrientations.LEFT_PADDED, job.ProgramId),
-                    _boolConverter.Convert(job.AutoSelect),
+                    _intConverter.Convert((int)job.AutoSelect),
                     _intConverter.Convert('0', 2, DataField.PaddingOrientations.LEFT_PADDED, job.BatchSize),
                     _intConverter.Convert('0', 2, DataField.PaddingOrientations.LEFT_PADDED, job.MaxCoherentNok)
                 };
 
-                if (_revision == 999)
+                if (_revision > 1)
+                {
                     fields.Add(_intConverter.Convert('0', 2, DataField.PaddingOrientations.LEFT_PADDED, job.BatchCounter));
+                    if (_revision != 999)
+                    {
+                        fields.Add(_intConverter.Convert('0', 4, DataField.PaddingOrientations.LEFT_PADDED, job.IdentifierNumber));
+                        fields.Add(GetPadded(' ', 25, DataField.PaddingOrientations.RIGHT_PADDED, job.JobStepName));
+                        fields.Add(_intConverter.Convert('0', 2, DataField.PaddingOrientations.LEFT_PADDED, job.JobStepType));
+                        if (_revision == 3)
+                        {
+                            fields.Add(_intConverter.Convert((int)job.ToolLoosening));
+                            fields.Add(_intConverter.Convert((int)job.JobBatchMode));
+                            fields.Add(_intConverter.Convert((int)job.BatchStatusAtIncrement));
+                            fields.Add(_intConverter.Convert((int)job.DecrementBatchAfterLoosening));
+                            fields.Add(_intConverter.Convert((int)job.CurrentBatchStatus));
+                        }
+                    }
+                }
                 advancedJobsList.Add(string.Join(":", fields));
             }
 
@@ -44,18 +59,38 @@ namespace OpenProtocolInterpreter.Converters
         public override IEnumerable<AdvancedJob> Convert(string value)
         {
             var list = value.Split(';');
-            foreach(var advancedJob in list)
+            foreach (var advancedJob in list)
             {
                 var fields = advancedJob.Split(':');
-                yield return new AdvancedJob()
+                var obj = new AdvancedJob()
                 {
                     ChannelId = _intConverter.Convert(fields[0]),
                     ProgramId = _intConverter.Convert(fields[1]),
-                    AutoSelect = _boolConverter.Convert(fields[2]),
+                    AutoSelect = (AutoSelect)_intConverter.Convert(fields[2]),
                     BatchSize = _intConverter.Convert(fields[3]),
-                    MaxCoherentNok = _intConverter.Convert(fields[4]),
-                    BatchCounter = _revision == 999 ? _intConverter.Convert(fields[5]) : 0
+                    MaxCoherentNok = _intConverter.Convert(fields[4])
                 };
+
+                if (_revision > 1)
+                {
+                    obj.BatchCounter = _intConverter.Convert(fields[5]);
+                    if (_revision != 999)
+                    {
+                        obj.IdentifierNumber = _intConverter.Convert(fields[6]);
+                        obj.JobStepName = fields[7];
+                        obj.JobStepType = _intConverter.Convert(fields[8]);
+                        if (_revision == 3)
+                        {
+                            obj.ToolLoosening = (ToolLoosening)_intConverter.Convert(fields[9]);
+                            obj.JobBatchMode = (BatchMode)_intConverter.Convert(fields[10]);
+                            obj.BatchStatusAtIncrement = (BatchStatusAtIncrement)_intConverter.Convert(fields[11]);
+                            obj.DecrementBatchAfterLoosening = (DecrementBatchAfterLoosening)_intConverter.Convert(fields[12]);
+                            obj.CurrentBatchStatus = (CurrentBatchStatus)_intConverter.Convert(fields[13]);
+                        }
+                    }
+                }
+
+                yield return obj;
             }
         }
     }
