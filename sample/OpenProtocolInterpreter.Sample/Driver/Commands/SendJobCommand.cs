d082e88a94a23124cc73d3c83a06f4a9ef3caf02
Henrique Dal Bello <henrique.d.bello@gmail.com>
2023-06-14 23:19:29 -0300

    Updated to version 6.0.0

--- sample/OpenProtocolInterpreter.Sample/Driver/Commands/SendJobCommand.cs
+++ sample/OpenProtocolInterpreter.Sample/Driver/Commands/SendJobCommand.cs
@@ -18,7 +18,7 @@ namespace OpenProtocolInterpreter.Sample.Driver.Commands
             Console.WriteLine($"Sending job <{jobId}> to controller!");
             var mid = _driver.SendAndWaitForResponse(new Mid0038(jobId).Pack(), new TimeSpan(0, 0, 10));
 
-            if (mid.HeaderData.Mid == Mid0004.MID)
+            if (mid.Header.Mid == Mid0004.MID)
             {
                 OnJobRefused(mid as Mid0004);
                 return false;
