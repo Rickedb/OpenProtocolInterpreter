d3d615de1d420f96d6f183a53c569eb623dab903
Henrique Dal Bello <henrique.d.bello@gmail.com>
2023-11-16 23:23:35 -0300

    #108 Added mid 0104

---
+++ src/MIDTesters.Core/MultiSpindle/TestMid0104.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenProtocolInterpreter.MultiSpindle;
+
+namespace MIDTesters.Core.MultiSpindle
+{
+    [TestClass]
+    [TestCategory("MultiSpindle")]
+    public class TestMid0104 : DefaultMidTests<Mid0104>
+    {
+        [TestMethod]
+        [TestCategory("Revision 1"), TestCategory("ASCII")]
+        public void Mid0104Revision1()
+        {
+            string pack = @"00320104            013009021015";
+            var mid = _midInterpreter.Parse<Mid0104>(pack);
+
+            Assert.AreNotEqual(0, mid.RequestedResultIndex);
+            AssertEqualPackages(pack, mid, true);
+        }
+
+        [TestMethod]
+        [TestCategory("Revision 1"), TestCategory("ByteArray")]
+        public void Mid0104ByteRevision1()
+        {
+            string package = @"00320104            013009021015";
+            byte[] bytes = GetAsciiBytes(package);
+            var mid = _midInterpreter.Parse<Mid0104>(bytes);
+
+            Assert.AreNotEqual(0, mid.RequestedResultIndex);
+            AssertEqualPackages(bytes, mid, true);
+        }
+    }
+}
---
+++ src/OpenProtocolInterpreter/MultiSpindle/Mid0104.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenProtocolInterpreter.MultiSpindle
+{
+    /// <summary>
+    /// Old Multi spindle result request
+    /// <para>Request for an old multi spindle result.</para>
+    /// <para>Note: MID 0104 can’t be used if there is an active subscription for multiple spindle results.</para>
+    /// <para>Message sent by: Integrator</para>
+    /// <para>
+    /// Answer: <see cref="Mid0101"/> Command accepted or 
+    /// <see cref="Communication.Mid0004"/> Command error, Multi spindle result subscription already exists
+    /// </para>
+    /// </summary>
+    public class Mid0104 : Mid, IMultiSpindle, IIntegrator, IAnswerableBy<Mid0101>, IDeclinableCommand
+    {
+        public const int MID = 104;
+
+        public IEnumerable<Error> DocumentedPossibleErrors => new Error[] { Error.MultiSpindleResultSubscriptionAlreadyExists };
+
+        public long RequestedResultIndex
+        {
+            get => GetField(1, DataFields.RequestedResultIndex).GetValue(OpenProtocolConvert.ToInt64);
+            set => GetField(1, DataFields.RequestedResultIndex).SetValue(OpenProtocolConvert.ToString, value);
+        }
+
+        public Mid0104() : base(MID, DEFAULT_REVISION) { }
+
+        public Mid0104(Header header) : base(header)
+        {
+        }
+
+        protected override Dictionary<int, List<DataField>> RegisterDatafields()
+        {
+            return new Dictionary<int, List<DataField>>()
+            {
+                {
+                    1, new List<DataField>()
+                            {
+                                DataField.Number(DataFields.RequestedResultIndex, 20, 10),
+                            }
+                }
+            };
+        }
+
+        protected enum DataFields
+        {
+            RequestedResultIndex
+        }
+    }
+}
