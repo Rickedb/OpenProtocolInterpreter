aaa4f8b513fde98caa3a10601d8a127d60618cd0
Henrique Dal Bello <henrique.d.bello@gmail.com>
2026-04-21 11:58:33 -0300

    performance tests

--- src/MIDTesters.Core/MIDsTests.cs
+++ src/MIDTesters.Core/MIDsTests.cs
@@ -1,5 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenProtocolInterpreter;
 using OpenProtocolInterpreter.Communication;
 using OpenProtocolInterpreter.MotorTuning;
@@ -40,7 +41,7 @@ namespace MIDTesters
             _allMidInterpreter = new MidInterpreter().UseAllMessages();
         }
 
-        [Benchmark(Description = "Build MidInterpreter (custom MIDs)")]
+        //[Benchmark(Description = "Build MidInterpreter (custom MIDs)")]
         public MidInterpreter BuildCustomMidInterpreter() =>
             new MidInterpreter().UseAllMessages(new Type[]
             {
@@ -56,16 +57,41 @@ namespace MIDTesters
                 typeof(Mid0504)
             });
 
-        [Benchmark(Description = "Build MidInterpreter (all MIDs)")]
+        //[Benchmark(Description = "Build MidInterpreter (all MIDs)")]
         public MidInterpreter BuildAllMidInterpreter() =>
             new MidInterpreter().UseAllMessages();
 
-        [Benchmark(Description = "Parse Mid0061 (custom MIDs)")]
+        //[Benchmark(Description = "Parse Mid0061 (custom MIDs)")]
         public Mid0061 ParseMid61CustomInterpreter() =>
             _customMidInterpreter.Parse<Mid0061>(Mid61Raw);
 
-        [Benchmark(Description = "Parse Mid0061 (all MIDs)", Baseline = true)]
+        //[Benchmark(Description = "Parse Mid0061 (all MIDs)", Baseline = true)]
         public Mid0061 ParseMid61AllInterpreter() =>
             _allMidInterpreter.Parse<Mid0061>(Mid61Raw);
+
+        [Benchmark(Description = "Mid performance")]
+        public Mid MidPerformance()
+        {
+            var value = "02240002007         010001020103Airbag1                  04ACT05OpenProtocolVersion06Version 19.0.0.0   07Version 01.0.0.0   08RBUType                 09Serial    100021100212013114429496729515Station Or Cell Name     16A171";
+            var mid = _allMidInterpreter.Parse<Mid0002>(value);
+            Assert.AreEqual(1, mid.CellId);
+            Assert.AreEqual(1, mid.ChannelId);
+            Assert.AreEqual("Airbag1", mid.ControllerName.TrimEnd());
+            Assert.AreEqual("ACT", mid.SupplierCode);
+            Assert.AreEqual("OpenProtocolVersion", mid.OpenProtocolVersion);
+            Assert.AreEqual("Version 19.0.0.0", mid.ControllerSoftwareVersion.TrimEnd());
+            Assert.AreEqual("Version 01.0.0.0", mid.ToolSoftwareVersion.TrimEnd());
+            Assert.AreEqual("RBUType", mid.RBUType.TrimEnd());
+            Assert.AreEqual("Serial", mid.ControllerSerialNumber.TrimEnd());
+            Assert.AreEqual(SystemType.PowerMacs4000, mid.SystemType);
+            Assert.AreEqual(SystemSubType.SystemRunningPresses, mid.SystemSubType);
+            Assert.IsFalse(mid.SequenceNumberSupport);
+            Assert.IsTrue(mid.LinkingHandlingSupport);
+            Assert.AreEqual(4294967295L, mid.StationCellId);
+            Assert.AreEqual("Station Or Cell Name", mid.StationCellName.TrimEnd());
+            Assert.AreEqual("A", mid.ClientId);
+            Assert.IsTrue(mid.OptionalKeepAlive);
+            return mid;
+        }
     }
 }
