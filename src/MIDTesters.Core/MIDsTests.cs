65c9576ac9309042cb4784519715691009da8bb6
Henrique Dal Bello <henrique.d.bello@gmail.com>
2026-04-21 11:41:16 -0300

    Added BenchmarkDotNet

--- src/MIDTesters.Core/MIDsTests.cs
+++ src/MIDTesters.Core/MIDsTests.cs
@@ -1,26 +1,48 @@
-﻿using OpenProtocolInterpreter;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using OpenProtocolInterpreter;
 using OpenProtocolInterpreter.Communication;
 using OpenProtocolInterpreter.MotorTuning;
 using OpenProtocolInterpreter.Tightening;
 using System;
-using System.Diagnostics;
 
 namespace MIDTesters
 {
-    //[Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    [MemoryDiagnoser]
     public class MIDsTests
     {
-        //[Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
-        public void TestProcessingTime()
+        private const string Mid61Raw =
+            "02310061001         010001020103airbag7                  04KPOL3456JKLO897          " +
+            "05000600307000008000009010011112000840130014001400120015000739160000017099991800000" +
+            "1900000202001-06-02:09:54:09212001-05-29:12:34:3322123345675    ";
+
+        private MidInterpreter _customMidInterpreter = null!;
+        private MidInterpreter _allMidInterpreter = null!;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            long total = 0;
-            string mid61 = "02310061001         010001020103airbag7                  04KPOL3456JKLO897          " +
-                           "05000600307000008000009010011112000840130014001400120015000739160000017099991800000" +
-                           "1900000202001-06-02:09:54:09212001-05-29:12:34:3322123345675    ";
-            //CustomMids
-            var watch = Stopwatch.StartNew();
-            var myTEmplate = new MidInterpreter()
+            _customMidInterpreter = new MidInterpreter()
                 .UseAllMessages(new Type[]
+                {
+                    typeof(Mid0001),
+                    typeof(Mid0002),
+                    typeof(Mid0003),
+                    typeof(Mid0004),
+                    typeof(Mid0061),
+                    typeof(Mid0500),
+                    typeof(Mid0501),
+                    typeof(Mid0502),
+                    typeof(Mid0503),
+                    typeof(Mid0504)
+                });
+
+            _allMidInterpreter = new MidInterpreter().UseAllMessages();
+        }
+
+        [Benchmark(Description = "Build MidInterpreter (custom MIDs)")]
+        public MidInterpreter BuildCustomMidInterpreter() =>
+            new MidInterpreter().UseAllMessages(new Type[]
             {
                 typeof(Mid0001),
                 typeof(Mid0002),
@@ -33,37 +55,17 @@ namespace MIDTesters
                 typeof(Mid0503),
                 typeof(Mid0504)
             });
-            watch.Stop();
-            Debug.WriteLine("[CustomMIDs] Elapsed time to construct MidInterpreter: " + new TimeSpan(watch.ElapsedTicks));
 
-            for (int i = 0; i < 1000000; i++)
-            {
+        [Benchmark(Description = "Build MidInterpreter (all MIDs)")]
+        public MidInterpreter BuildAllMidInterpreter() =>
+            new MidInterpreter().UseAllMessages();
 
-                watch.Restart();
-                var myMid106 = myTEmplate.Parse<Mid0061>(mid61);
-                watch.Stop();
-                total += watch.ElapsedTicks;
-            }
-            Debug.WriteLine($"[CustomMIDs] Total Elapsed: " + new TimeSpan(total));
-            Debug.WriteLine($"[CustomMIDs] Average Elapsed Time: " + new TimeSpan(total / 1000000));
+        [Benchmark(Description = "Parse Mid0061 (custom MIDs)")]
+        public Mid0061 ParseMid61CustomInterpreter() =>
+            _customMidInterpreter.Parse<Mid0061>(Mid61Raw);
 
-            //All MIDs
-            watch.Start();
-            myTEmplate = new MidInterpreter().UseAllMessages();
-            watch.Stop();
-            Debug.WriteLine("[AllMIDs] Elapsed time to construct MidInterpreter: " + new TimeSpan(watch.ElapsedTicks));
-
-            total = 0;
-            for (int i = 0; i < 1000000; i++)
-            {
-                watch.Restart();
-                var myMid500 = myTEmplate.Parse<Mid0061>(mid61);
-                watch.Stop();
-                total += watch.ElapsedTicks;
-            }
-
-            Debug.WriteLine($"[AllMIDs] Total Elapsed: " + new TimeSpan(total));
-            Debug.WriteLine($"[AllMIDs] Average Elapsed Time: " + new TimeSpan(total / 1000000));
-        }
+        [Benchmark(Description = "Parse Mid0061 (all MIDs)", Baseline = true)]
+        public Mid0061 ParseMid61AllInterpreter() =>
+            _allMidInterpreter.Parse<Mid0061>(Mid61Raw);
     }
 }
---
+++ src/MIDTesters.Core/Program.cs
@@ -0,0 +1,4 @@
+using BenchmarkDotNet.Running;
+using MIDTesters;
+
+BenchmarkRunner.Run<MIDsTests>();
