235a3e04302fb9ce35767196756554a439857754
Henrique Dal Bello <henrique.d.bello@gmail.com>
2026-07-14 23:56:58 -0300

    updated OpenProtocolCommandsDisable to use datafield attributes

--- src/OpenProtocolInterpreter/OpenProtocolCommandsDisabled/Mid0421.cs
+++ src/OpenProtocolInterpreter/OpenProtocolCommandsDisabled/Mid0421.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace OpenProtocolInterpreter.OpenProtocolCommandsDisabled
 {
     /// <summary>
     /// Open Protocol commands disabled
     /// <para>
-    ///     Upload the status of the Open Protocol commands disable digital input. 
-    ///     The data upload consists of one byte delivering the digital input status. 
-    ///     The status is uploaded each time the “Open Protocol commands disable” digital 
+    ///     Upload the status of the Open Protocol commands disable digital input.
+    ///     The data upload consists of one byte delivering the digital input status.
+    ///     The status is uploaded each time the “Open Protocol commands disable” digital
     ///     input changes (push function).
-    /// </para>    
+    /// </para>
     /// <para>Message sent by: Controller</para>
     /// <para>Answer: <see cref="Mid0422"/> Open Protocol commands disabled acknowledge</para>
     /// </summary>
@@ -17,12 +18,8 @@ namespace OpenProtocolInterpreter.OpenProtocolCommandsDisabled
     {
         public const int MID = 421;
 
-        public bool DigitalInputStatus
-        {
-            get => GetField(1, DataFields.DigitalInputStatus).GetValue(OpenProtocolConvert.ToBoolean);
-            set => GetField(1, DataFields.DigitalInputStatus).SetValue(OpenProtocolConvert.ToString, value);
-        }
-
+        [BooleanDataFieldDefinition(revision: 1, field: 1, Index = 20, HasPrefix = false)]
+        public bool DigitalInputStatus { get; set; }
         public Mid0421() : this(new Header()
         {
             Mid = MID,
@@ -35,19 +32,7 @@ namespace OpenProtocolInterpreter.OpenProtocolCommandsDisabled
         {
         }
 
-        protected override Dictionary<int, List<DataField>> RegisterDatafields()
-        {
-            return new Dictionary<int, List<DataField>>()
-            {
-                {
-                    1, new List<DataField>()
-                            {
-                                DataField.Boolean(DataFields.DigitalInputStatus, 20, false)
-                            }
-                }
-            };
-        }
-
+        [Obsolete("Use DataFieldDefinition attributes instead")]
         protected enum DataFields
         {
             DigitalInputStatus
